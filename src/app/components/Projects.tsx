e1f7ddb5f560a1092004e6a3dc2eae3aac90a255
Marissa Vargas <marissa.lorena@hotmail.com>
2024-04-03 13:28:37 -0600

    add: pictures for projects, projects and status key

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -19,19 +19,22 @@ export default function Projects() {
       </div>
       <div className="px-8">
         {data.projects.map((project, index) => (
-          <div
-            key={index}
-            className="mb-8 grid sm:grid-cols-4 py-4"
-          >
+          <div key={index} className="mb-8 grid sm:grid-cols-4 py-4">
             <div className="sm:col-span-3 lg:ml-5">
-              <div className="group">
-                <a href={project.link} target="_blank" aria-label={project.linkAlt} className="flex items-center">
-                <LinkSvg className="fill-moody-blue-200/90 mr-2 group-hover:fill-moody-blue-400 group-hover:w-5 group-hover:h-5 transition-all" />
+              <div className="group flex items-center">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  aria-label={project.linkAlt}
+                  className="flex items-center"
+                >
+                  <LinkSvg className="fill-moody-blue-200/90 mr-2 group-hover:fill-moody-blue-400 group-hover:w-5 group-hover:h-5 transition-all" />
                   <h4 className="font-bold text-moody-blue-200 tracking-wider text-lg group-hover:text-moody-blue-400 group-hover:text-xl transition-all">
                     {project.title}
                   </h4>
                 </a>
               </div>
+              {project.status && <p className={`${spline.className} bg-moody-blue-300 text-moody-blue-950 font-medium tracking-wider block w-fit rounded-full py-1 px-3 m-1 text-xs`}>{project.status}</p>}
               <p className="py-2 font-extralight leading-relaxed">
                 {project.description}
               </p>
@@ -46,8 +49,14 @@ export default function Projects() {
                 ))}
               </ul>
             </div>
-            <div className="sm:order-first">
-              <Image src={project.image} width={120} height={70} alt={project.imgAlt} className="rounded-md sm:my-2"/>
+            <div className="sm:order-first ">
+              <Image
+                src={project.image}
+                width={120}
+                height={70}
+                alt={project.imgAlt}
+                className="rounded-md sm:my-2 border-2 border-moody-blue-300/50 transition"
+              />
             </div>
           </div>
         ))}
